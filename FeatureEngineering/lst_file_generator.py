cfa6cf7b62fdafe3a813d89f530d5eccbd1ade55
sindhuPyxeda <sindhu@pyxeda.ai>
2020-06-16 17:54:55 -0700

    lst files for test and train

--- FeatureEngineering/lst_file_generator.py
+++ FeatureEngineering/lst_file_generator.py
@@ -1,20 +1,29 @@
 # Generate lst files given the folder containing sub-folders of images
 import os
-folder = '<your_path>/Cats_and_Dogs'
+import random
+
+folder = '<>/Cats_and_Dogs'
 subfolders = [f.path for f in os.scandir(folder) if f.is_dir()]
 
-# Create a lst file
-f = open("my_lst_file.lst", "w")
+# Create a train and test lst file
+f_train = open("my_lst_train_file.lst", "w")
+f_test = open("my_lst_test_file.lst", "w")
 image_index = 0
 label_index = 0
 label_encoding = {}
+num_samples = len(subfolders)
 for name in subfolders:
     label = name.split('/')[-1]
     label_encoding[label_index] = label
     onlyfiles = [f for f in os.listdir(name) if os.path.isfile(os.path.join(name, f))]
     for image_names in onlyfiles:
-        f.write(str(image_index) + '\t'+ str(label_index) + '\t'+ label+ '/' + image_names + '\n')
+        # Randomly write 20% of the files into test lst
+        if (random.uniform(0, 1) < 0.8):
+            f_train.write(str(image_index) + '\t'+ str(label_index) + '\t'+ label+ '/' + image_names + '\n')
+        else:
+            f_test.write(str(image_index) + '\t'+ str(label_index) + '\t'+ label+ '/' + image_names + '\n')
         image_index += 1
     label_index += 1
-f.close()
+f_train.close()
+f_test.close()
 print(label_encoding)
